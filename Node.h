19b084decdbbc76b01f21c45a4de9d33d11ae5b9
Cristhyan Perez <pevicristhyan@gmail.com>
2024-12-14 09:27:11 -0500

    Class Node - NumberNode

--- Node.h
+++ Node.h
@@ -6,9 +6,19 @@
 
 template<typename T>
 class Node {
-public:
-    virtual ~Node() {}
-    virtual T evaluate(const std::map<std::string, std::unique_ptr<Node<T>>> &cellExpressions) const = 0;
+    public:
+        virtual ~Node() {}
+        virtual T evaluate(const std::map<std::string, std::unique_ptr<Node<T>>> &cellExpressions) const = 0;
+};
+
+template<typename T>
+class NumberNode : public Node<T> {
+        T value;
+    public:
+        NumberNode(T v) : value(v) {}
+        T evaluate(const std::map<std::string, std::unique_ptr<Node<T>>> &) const override {
+            return value;
+        }
 };
 
 #endif
