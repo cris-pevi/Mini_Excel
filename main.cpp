b1d6dfa84d64156e6cb7f2722523475629aaa9fb
Cristhyan Perez <pevicristhyan@gmail.com>
2024-12-14 09:27:32 -0500

    Probar el NumberNode

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
+#include <iostream>
 #include "Node.h"
 
 int main() {
+    // Nodo que contiene un número
+    NumberNode<double> numNode(42.5);
+
+    // Mapa vacío
+    std::map<std::string, std::unique_ptr<Node<double>>> cellExpressions;
+
+    // Evaluación del nodo
+    std::cout << "El valor del nodo es: " << numNode.evaluate(cellExpressions) << std::endl;
+
     return 0;
 }
